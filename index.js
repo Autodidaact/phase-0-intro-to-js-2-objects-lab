45665d0f587024b1bd81e8cee61c7c3e650cb0e1
Ibrahim Abdulai Yakubu <ibrahimay295@gmail.com>
2022-06-12 10:53:19 +0000

    Modified index.js

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+let employee = {
+    'name': 'Aku',
+    'streetAddress': '5 Nii Amui'
+}
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    return {...employee,
+    [key]: value
+    };
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key){
+    const clonEmployee = {...employee};
+    delete clonEmployee[key];
+    return clonEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
